220ea49549b0083f98da19b2b24878d7ff94d9db
Diego Alvarado <diego23k@gmail.com>
2018-04-23 17:02:11 +0200

    set up create react app

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {render} from 'react-dom';
+import './index.css';
+import Router from "./components/Router";
+import registerServiceWorker from './registerServiceWorker';
+
+render(<Router />, document.getElementById('root'));
+registerServiceWorker();
